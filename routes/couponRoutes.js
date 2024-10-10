53fc51f104944d723c6b38c04d2b52ab974173b9
ayush kumar <ayush2422005@gmail.com>
2024-10-10 10:23:40 +0530

    updated get profile

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const { create, getall, deleteCoupon, getbyid } = require('../controllers/couponController');
+const { create, getall, deleteCoupon, getbyid, toggleActive } = require('../controllers/couponController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/create', authMiddleware, create);
-// router.post('/update', login);
-// router.post('/delete', signout);
 router.get('/getall', authMiddleware, getall);
 router.get('/get/:id', authMiddleware, getbyid);
 router.delete('/delete/:id', authMiddleware, deleteCoupon);
+
+
+// updation 
+router.put('/toggle-active/:id', authMiddleware, toggleActive);
+// router.put('/update', authMiddleware, updateCou);
 
 module.exports = router;
