1beb758e870aa9e058db893745ab8785687d27ae
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-10-10 10:02:25 +0530

    code setup

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // No need for deprecated options
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
